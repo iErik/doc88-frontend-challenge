4899a66631ba0d82a46ec88a6b827c2c5f08a686
Erik Isidore <ericssonico@protonmail.ch>
2019-12-22 21:56:23 -0300

    Fix typo

--- tests/mock/matchMedia.js
+++ tests/mock/matchMedia.js
@@ -1,6 +1,6 @@
 /**
  * window.matchMedia (which is used by our MediaQuery mixin) is not
- * implemented in JSOM by default, this is a workaround so that we
+ * implemented in JSDOM by default, this is a workaround so that we
  * can still mount components that use this method.
  * you can find more information about it here: http://tiny.cc/ry9xhz
  */
